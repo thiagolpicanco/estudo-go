c872bc0ac4e5cb320afb760225dcc4b84469a95b
Thiago Picanco <thiagolpicanco@gmail.com>
2018-02-06 10:07:17 -0200

    select nos canais

--- canais/canal.go
+++ canais/canal.go
@@ -5,13 +5,16 @@ import (
 	"time"
 )
 
+var irc = make(chan string)
+var sms = make(chan string)
+
 //TestaCanais testaCanais, um canal serve para mandar um sinal(msg) para outra rotina
 func TestaCanais() {
-	var canal chan string
-	canal = make(chan string)
-	go pinger(canal)
-	go ponger(canal)
-	go imprimir(canal)
+	go pinger(irc)
+	go ponger(irc)
+	go epa(sms)
+	go opa(sms)
+	go imprimir()
 	var entrada string
 	fmt.Scanln(&entrada)
 
@@ -29,10 +32,28 @@ func ponger(canal chan string) {
 	}
 }
 
-func imprimir(canal chan string) {
+func epa(canal chan string) {
 	for {
-		msg := <-canal
-		fmt.Println(msg)
+		canal <- "Epa"
+	}
+}
+
+func opa(canal chan string) {
+	for {
+		canal <- "Opa"
+	}
+}
+
+func imprimir() {
+	var msg string
+	for {
+		select {
+		case msg = <-irc:
+			fmt.Println(msg)
+			time.Sleep(time.Second * 1)
+		case msg = <-sms:
+			fmt.Println("chegou sms: ", msg)
+		}
 		time.Sleep(time.Second * 1)
 	}
 
